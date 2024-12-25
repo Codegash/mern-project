993857767fb557b10162861ce56694c9b38c2f1b
Codegash <123890709+Codegash@users.noreply.github.com>
2024-12-25 14:50:25 +0530

    changed style

--- bluedigital/src/App.jsx
+++ bluedigital/src/App.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-import Navbar from "./components/submenu/submenu";
-import Section1 from "./components/section1/section1";
-import Section2 from "./components/section2/section2";
 import "./App.css";
 const App = () => {
   return (
